4787e0089cd3699042d919ca9d013e9c4f04075a
MisterDonMichi <felixgonzalezlds@gmail.com>
2026-09-06 16:54:13 -0300

    ex1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        Console.Write("What is your first name?: ");
+        string name = Console.ReadLine();
     }
 }
